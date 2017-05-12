b4c91e44fbe1c1e9421606b1f47e2f6b48e5f6a2
Oren Farhi <farhioren@gmail.com>
2017-05-12 08:34:23 +0300

    udpated exports

--- src/ng2-youtube-player.ts
+++ src/ng2-youtube-player.ts
@@ -3,5 +3,5 @@ export { YoutubePlayerModule } from './modules/ng2-youtube-player.module';
 export { YoutubePlayerComponent } from './modules/youtube-player.component';
 
 export {
-  Key
+  PlayerApiScriptOptions, PlayerOutputs, PlayerSize
 } from './models';
