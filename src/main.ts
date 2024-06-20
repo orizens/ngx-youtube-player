af4546d6919e718910aeb79d96ce8302c4866ce2
Oren Farhi <farhioren@gmail.com>
2024-06-20 12:05:27 -0400

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-// import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
